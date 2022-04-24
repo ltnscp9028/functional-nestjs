d50f5846d555c9023a19fe9cd48f0880862a910a
y0on2q <ltnscp9028@gmail.com>
2022-04-24 19:34:32 +0900

    Implement: prisma service

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global()
 @Module({
     providers: [PrismaService],
+    exports: [PrismaService],
 })
 export class PrismaModule {}
