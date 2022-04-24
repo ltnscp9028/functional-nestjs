8c311b9b018783ca4c30dc3850bcf40b36899823
y0on2q <39849430+ltnscp9028@users.noreply.github.com>
2022-04-25 00:35:12 +0900

    Revert "Implement: prisma service"
    This reverts commit 76891df8fac88cbc6b9e715c44bc7eb14abf3c7f.

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,7 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-@Global()
 @Module({
     providers: [PrismaService],
-    exports: [PrismaService],
 })
 export class PrismaModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-    constructor() {
-        super();
-    }
-}
+export class PrismaService {}
