cd3198f20aa425b00448b0d44b7b6c11e1025a46
y0on2q <39849430+ltnscp9028@users.noreply.github.com>
2022-04-25 00:35:12 +0900

    Revert "Implement: explicit prisma connect"
    This reverts commit 9c9d6470d2f856f78d33ff4c481f4e1ad677169c.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,25 +1,9 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-    extends PrismaClient
-    implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient {
     constructor() {
-        super({
-            log: [
-                { emit: 'stdout', level: 'query' },
-                { emit: 'stdout', level: 'error' },
-            ],
-        });
-    }
-
-    async onModuleInit() {
-        await this.$connect();
-    }
-
-    async onModuleDestroy() {
-        await this.$disconnect();
+        super();
     }
 }
