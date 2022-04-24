096d1be014e5796875f206c99742eac02fab1e9f
y0on2q <ltnscp9028@gmail.com>
2022-04-24 23:26:32 +0900

    Implement: explicit prisma connect

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+{
     constructor() {
-        super();
+        super({
+            log: [
+                { emit: 'stdout', level: 'query' },
+                { emit: 'stdout', level: 'error' },
+            ],
+        });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
     }
 }
