76891df8fac88cbc6b9e715c44bc7eb14abf3c7f
y0on2q <ltnscp9028@gmail.com>
2022-04-24 19:34:32 +0900

    Implement: prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+    constructor() {
+        super();
+    }
+}
