848aeeb24706a4cf16ec5026ba997904f9deb78c
y0on2q <39849430+ltnscp9028@users.noreply.github.com>
2022-04-25 00:35:12 +0900

    Revert "Init: User Module"
    This reverts commit 2ca985aeb13c94675421685a51fe1e646380651f.

--- src/user/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-
-@Module({
-    providers: [UserService],
-    controllers: [UserController],
-})
-export class UserModule {}
