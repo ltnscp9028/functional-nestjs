84852948006259d8af128bd10118cfa0129d7350
y0on2q <39849430+ltnscp9028@users.noreply.github.com>
2022-04-25 00:35:12 +0900

    Revert "Init: prisma directory"
    This reverts commit 379b3827b1f7c215aec074e62c9b2f0bcdb9cc6a.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [],
     controllers: [AppController],
     providers: [AppService],
 })
